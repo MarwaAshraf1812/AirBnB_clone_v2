219b7e6ce22d11858b0353a1540257e5dbd4f7b7
MarwaAshraf1812 <marashraf090@gmail.com>
2024-02-04 18:57:09 +0200

    Console improvements

--- console.py
+++ console.py
@@ -115,7 +115,6 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        print("Debug: args =", args)
         if not args:
             raise SyntaxError()
         args_list = args.split(' ')
