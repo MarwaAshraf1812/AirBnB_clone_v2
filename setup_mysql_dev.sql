6a16ba2bd3001547937230b4470c7a2657962597
MarwaAshraf1812 <marashraf090@gmail.com>
2024-02-04 20:49:05 +0200

    MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,9 +7,6 @@
 -- Create or ensure the existence of the 'hbnb_dev_db' database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
--- Set the password policy to LOW for simplicity
-SET GLOBAL validate_password.policy = LOW;
-
 -- Create or ensure the existence of the 'hbnb_dev' user with the specified password
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
@@ -18,6 +15,3 @@ GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- Grant SELECT privilege on the 'performance_schema' database to the 'hbnb_dev' user
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-
--- Refresh MySQL privileges to apply the changes
-FLUSH PRIVILEGES;
