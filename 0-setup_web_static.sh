54cb16e0fe9b07aca1febcbc29fec109767c398c
MarwaAshraf1812 <marashraf090@gmail.com>
2024-02-07 00:50:35 +0200

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 # script that sets up web servers for the deployment of web_static
-
-# Install Nginx if not already installed
 sudo apt-get update
 sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
-# Create necessary directories
-sudo mkdir -p  /data/
+
+sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p  /data/web_static/shared/
+sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
 sudo echo "<html>
@@ -19,9 +17,11 @@ sudo echo "<html>
     Holberton School
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -s -f /data/web_static/releases/test/  /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
-sudo service nginx restart
-echo -e "Completed\n"
 
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
+sudo service nginx restart
