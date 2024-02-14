8dbbba96433cf49052a4c9b6a1e0c811d393be75
MarwaAshraf1812 <marashraf090@gmail.com>
2024-02-14 03:05:03 +0200

    Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+a Fabric script that generates a .tgz archive
+from the contents of the web_static folder of
+"""
+
+from fabric.api import *
+from datetime import datetime
+import os
+
+@task
+def do_pack():
+    """
+    generates a .tgz archive
+    from the contents of the web_static folder
+    """
+
+    local("sudo mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    archived_path = "veronsis/web_static_{}.tgz".format(date)
+    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_path))
+    file_size = os.path.getsize(archived_path)
+    print(file_size)
+    if t_gzip_archive.succeeded:
+        return archived_path
+    else:
+        return None
