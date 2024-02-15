506bb8f438dbf67f6398ad80f96191b20521b69f
MarwaAshraf1812 <marashraf090@gmail.com>
2024-02-15 17:58:25 +0200

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,26 +8,30 @@ from fabric.api import put, run, env
 from os.path import exists
 env.hosts = ["18.209.224.234", "52.23.213.75"]
 
+
 def do_deploy(archive_path):
     """
     deployes web static 1
     """
     if exists(archive_path):
         f_name = archive_path.split("/")[-1]
-        archive_no_ext = f_name.split(".")[0]
+        archive_ext = f_name.split(".")[0]
         r_release = "/data/web_static/releases"
         try:
             put(archive_path, '/tmp/')
-            run('sudo mkdir -p {}/{}/'.format(r_release, archive_no_ext))
-            run('sudo tar -xzf /tmp/{} -C {}/{}/'.format(f_name, r_release, archive_no_ext))
+            run('sudo mkdir -p {}/{}/'.format(r_release, archive_ext))
+            run('sudo tar -xzf /tmp/{} -C {}/{}/'.format(f_name,
+                                                         r_release,
+                                                         archive_ext))
             run("sudo mv {}/{}/web_static/* {}/{}/".format(r_release,
-                                                           archive_no_ext,
+                                                           archive_ext,
                                                            r_release,
-                                                           archive_no_ext))
-            run ("sudo rm -r {}/{}/web_static".format(r_release, archive_no_ext))
+                                                           archive_ext))
+            run("sudo rm -r {}/{}/web_static".format(r_release, archive_ext))
             run('sudo rm /tmp/{}'.format(f_name))
             run('sudo rm -f /data/web_static/current')
-            run('sudo ln -s {}/{} /data/web_static/current'.format(r_release, archive_no_ext))
+            run('sudo ln -s {}/{} /data/web_static/current'.format(
+                r_release, archive_ext))
             print('New version deployed!')
             return True
         except:
